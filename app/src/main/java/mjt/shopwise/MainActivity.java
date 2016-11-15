b29f24c79469eccefc0a66a99e96f26605425817
MJT11111 <mjt11111@yandex.com>
2016-11-16 07:08:12 +1100

    Initial

---
+++ app/src/main/java/mjt/shopwise/MainActivity.java
@@ -0,0 +1,16 @@
+package mjt.shopwise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Main/Start Activity for ShopWise
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
